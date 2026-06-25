60c6da46b73f7d93cb4b41d6aa6007278726d89d
Suzuki40404 <Suzuki40404@users.noreply.github.com>
2026-06-25 05:14:49 +0000

    deploy: 21b77796202cc2e71ef7431c80385f38eb4c40bd

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -51,7 +51,7 @@ if (flet.noCdn) {
 _flutter.loader.load({
     config: flutterConfig,
     serviceWorkerSettings: {
-        serviceWorkerVersion: "3726909999" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */,
+        serviceWorkerVersion: "3462932521" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */,
     },
     onEntrypointLoaded: async function (engineInitializer) {
         const engine = await engineInitializer.initializeEngine(flutterConfig);
